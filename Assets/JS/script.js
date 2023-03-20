27e9fed7d6909f5c7594a59cdc979b1a1448c424
JustinBugarin <124007639+JustinBugarin@users.noreply.github.com>
2023-03-20 18:42:13 -0500

    Merge branch 'main' of https://github.com/Bocabooii/Beat-Spotify

---
+++ Assets/JS/script.js
@@ -0,0 +1,15 @@
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2d1ed2a192msh98939cb7c479eaap1118bajsn8c8e3a534674',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	}
+};
+
+fetch('https://spotify23.p.rapidapi.com/track_lyrics/?id=1brwdYwjltrJo7WHpIvbYt', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+    console.log("This works")
